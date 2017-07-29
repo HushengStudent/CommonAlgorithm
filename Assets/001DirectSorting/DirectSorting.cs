68a2bdc34c842f3f6aa7405d69a9bc3590f4d17c
HushengStudent <husheng.student@foxmail.com>
2017-07-30 00:45:55 +0800

    DirectSorting

---
+++ Assets/001DirectSorting/DirectSorting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectSorting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
