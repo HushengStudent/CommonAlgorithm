fdc0978628d4402b4f4d3a58d8aea76fe166b6a9
HushengStudent <husheng.student@foxmail.com>
2017-07-31 23:12:09 +0800

    SelectSorting finished

---
+++ Assets/002SelectSorting/SelectSorting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class SelectSorting 
+{
+    /// <summary>
+    /// 选择排序;
+    /// </summary>
+    /// <param name="list">无序List</param>
+    /// <returns>从小到大有序List</returns>
+    public static List<int> SortingIntSmall(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                if (list[i] > list[j])
+                {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        return list;
+    }
+}
