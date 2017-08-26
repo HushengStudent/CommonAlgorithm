9729c83771237e6c12a611a0d542d9d90fd63b4a
HushengStudent <husheng.student@foxmail.com>
2017-08-26 23:50:31 +0800

    FibonacciSequence finished

--- Assets/2Recursive/1FibonacciSequence/FibonacciSequence.cs
+++ Assets/2Recursive/1FibonacciSequence/FibonacciSequence.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class FibonacciSequence : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+/// <summary>
+/// F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
+/// </summary>
+public static class FibonacciSequence 
+{
+    public static int AccumulationFibonacci(int index)
+    {
+        if (index == 0) return 0;
+        if (index == 1) return 1;
+        return AccumulationFibonacci(index - 1) + AccumulationFibonacci(index - 2);
+    }
 }
